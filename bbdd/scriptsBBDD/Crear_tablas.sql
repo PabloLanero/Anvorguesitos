20f8f236e01848bd4a5b0c31df55f9ea803a0b93
Pablo Lanero <pablolaneroperez@gmail.com>
2025-04-03 19:41:07 +0200

    Te añado unas pocas mas

--- bbdd/scriptsBBDD/Crear_tablas.sql
+++ bbdd/scriptsBBDD/Crear_tablas.sql
@@ -11,5 +11,23 @@ CREATE TABLE MENUS(
 CREATE TABLE PRODUCTS(
     id_product INT PRIMARY KEY,
     description VARCHAR2(250),
-    price DECIMAL(2),
+    price DECIMAL(9,2),
     id_productCategory INT);
+
+CREATE TABLE PRODUCT_CATEGORIES(
+    id_productCategory INT PRIMARY KEY,
+    name VARCHAR2(50));
+
+CREATE TABLE STORES(
+    id_store INT PRIMARY KEY,
+    modeStore VARCHAR2(30),
+    location VARCHAR2(150),
+    id_menu INT);
+
+CREATE TABLE CUSTOMERS(
+    id_customer INT PRIMARY KEY,
+    firstName VARCHAR2(150),
+    lastName VARCHAR2(150),
+    emailCustomer VARCHAR2(150),
+    phoneCustomer VARCHAR2(10),
+    isRegistered BOOLEAN);
